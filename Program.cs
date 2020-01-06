3cd8cba45b11bb27039e02a8b30d2e6bcbb677ba
Matt Obert on Windows <matt@hotwebmatter.com>
2020-01-06 10:45:15 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace professor_ratings
 {
@@ -6,7 +7,7 @@ namespace professor_ratings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
